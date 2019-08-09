8b3a581817dfa06eef06382dbf9b57c7e2d7dc48
Howard Chang <hschang.2008@gmail.com>
2019-08-09 17:46:20 +0800

    Add generic types, refine type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,24 @@
-interface Cacheable {
-  data: any
+declare type CachedData<T> = T
+declare type CachedProcess<T> = null | Pick<ReturnFx<T>, 'refresh'>
+declare type UserFunction<T> = () => Promise<T>
+
+declare interface ReturnFx<T> {
+  (): Promise<CachedData<T>>
+  refresh: () => Promise<UserFunction<T> | CachedProcess<T>>
+}
+
+declare interface Cacheable<T> extends ReturnFx<T> {
+  data: CachedData<T>
   loaded: boolean
-  process: ReturnFx
+  process: CachedProcess<T>
+  (fx?: UserFunction<T>): Promise<CachedData<T>>
 }
 
-interface ReturnFx {
-  (): Promise<Pick<Cacheable, 'data'>>
-  refresh: () => Promise<UserFunction>
-}
-
-interface UserFunction {
-  (): Promise<any>
-}
-
-declare function Cacheable (fx: UserFunction): Promise<ReturnFx>
+/**
+ * Caches any data that is supplied by the user
+ * @param fx Defines a user given function
+ * @returns Returns a promise of the user given function
+ */
+declare function Cacheable<T>(fx: UserFunction<T>): Cacheable<T>
 
 export = Cacheable
